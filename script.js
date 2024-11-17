e25d914f2c1f105a6c2f526b4487b23606963026
mvpcurtis <curt.is@tuta.io>
2024-11-17 13:22:55 +0000

    go the basic functioning to work when receiving a number

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
 'use strict';
 
-const convBtn = document.querySelector('button');
+/*
+    1 meter = 3.281 feet
+    1 liter = 0.264 gallon
+    1 kilogram = 2.204 pound
+*/
 
-convBtn.addEventListener('click', function (event) {
-  console.log('Hello World!');
-  document.querySelector('h1').textContent = 'JS Linked!';
-});
+const convBtn = document.querySelector('button');
+const lengthParaEl = document.querySelector('#length > p');
+const volumeParaEl = document.querySelector('#volume > p');
+const massParaEl = document.querySelector('#mass > p');
+const inputEl = document.querySelector('input');
+
+const meterToFeet = function (num) {
+  return num * 3.281;
+};
+
+const feetToMeter = function (num) {
+  return num / 3.281;
+};
+
+const literToGallon = function (num) {
+  return num * 0.264;
+};
+
+const gallonToLiter = function (num) {
+  return num / 0.264;
+};
+
+const kiloToPound = function (num) {
+  return num * 2.204;
+};
+
+const poundToKilo = function (num) {
+  return num / 2.204;
+};
+
+const startConversion = function () {
+  const value = inputEl.value;
+
+  lengthParaEl.textContent = `
+    ${value} meters = ${meterToFeet(value).toFixed(2)} feet | 
+    ${value} feet = ${feetToMeter(value).toFixed(2)} meters
+`;
+
+  volumeParaEl.textContent = `
+    ${value} liters = ${literToGallon(value).toFixed(2)} gallons |
+    ${value} gallons = ${gallonToLiter(value).toFixed(2)} liters
+`;
+
+  massParaEl.textContent = `
+    ${value} kilos = ${kiloToPound(value).toFixed(2)} pounds |
+    ${value} pounds = ${poundToKilo(value).toFixed(2)} kilos
+`;
+};
+
+convBtn.addEventListener('click', startConversion);
